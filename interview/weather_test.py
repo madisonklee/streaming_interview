50efd3b7725cbf93ea8f31b7b89611b4d53e4d06
Madison Lee <madisonklee131@gmail.com>
2024-10-31 16:26:47 -0400

    Updated to improve make test score
    lots of whitespace deleted

--- interview/weather_test.py
+++ interview/weather_test.py
@@ -1,46 +1,6 @@
 from . import weather
 
-test_data1 = [
-    {"type": "sample", "stationName": "Foster Weather Station", "timestamp": 1672531200000, "temperature": 37.1}, 
-    {"type": "sample", "stationName": "Foster Weather Station", "timestamp": 1672531400000, "temperature": 32.5}, 
-    {"type": "control", "command": "snapshot"},
-    {"type": "control", "command": "reset"}
-]
-
-expected_output1 = [
-    {"type": "snapshot", "asOf": 1672531400000, "stations": {"Foster Weather Station": {"high": 37.1, "low": 32.5}}},
-    {"type": "reset", "asOf": 1672531400000}
-]
-
-test_data2 = [
-    {"type": "control", "command": "snapshot"},
-    {"type": "sample", "stationName": "Foster Weather Station", "timestamp": 1672531200000, "temperature": 37.1}, 
-    {"type": "sample", "stationName": "Foster Weather Station", "timestamp": 1672531400000, "temperature": 32.5}, 
-    {"type": "control", "command": "reset"}
-]
-
-expected_output2 = [
-    {"type" : "snapshot", "asOf" : "No as of value available with no station data", "stations" : "No station data available for snapshot."},
-    {"type": "reset", "asOf": 1672531400000}
-]
-
-test_data3 = [
-    {"type": "control", "command": "reset"},
-    {"type": "sample", "stationName": "Foster Weather Station", "timestamp": 1672531200000, "temperature": 37.1}, 
-    {"type": "sample", "stationName": "Foster Weather Station", "timestamp": 1672531400000, "temperature": 32.5}, 
-    {"type": "control", "command": "snapshot"},
-]
-
-expected_output3 = [
-    {"type" : "reset", "asOf" : "No as of value available with no station data."}, 
-    {"type": "snapshot", "asOf": 1672531400000, "stations": {"Foster Weather Station": {"high": 37.1, "low": 32.5}}},
-]
-
 def test_weather():
-    # print output of weather.py process_events
-    # print(list(weather.process_events(test_data2)))
-    
-    assert expected_output1 == list(weather.process_events(test_data1))
-    # print("Made it past test_weather's asset")
+    assert [{"type": "snapshot", "asOf": 1672531400000, "stations": {"Foster Weather Station": {"high": 37.1, "low": 32.5}}}, {"type": "reset", "asOf": 1672531400000}] == list(weather.process_events([{"type": "sample", "stationName": "Foster Weather Station", "timestamp": 1672531200000, "temperature": 37.1}, {"type": "sample", "stationName": "Foster Weather Station", "timestamp": 1672531400000, "temperature": 32.5}, {"type": "control", "command": "snapshot"}, {"type": "control", "command": "reset"}]))
 
 test_weather()
